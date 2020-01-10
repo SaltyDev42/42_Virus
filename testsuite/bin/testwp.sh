86cdb3f75d3d624e39ade0d3bff8777f7b6b15af
tdumouli <tdumouli@student.42.fr>
2020-01-10 20:52:08 +0100

    test case repair

--- testsuite/bin/testwp.sh
+++ testsuite/bin/testwp.sh
@@ -5,31 +5,43 @@ then
 	exit 1
 fi
 	gcc $2 -o /tmp/$1 testsuite/src/$1.c
-	echo "gcc $2 -o /tmp/_test_woody_$1 testsuite/src/$1.c" >>b
-	ls /tmp >>c
 
+rm woody
+./woody_woodpacker /tmp/$1 >> /dev/null
 
-./woody_woodpacker /tmp/$1 > a
+result=$?
 if [ $? -ne 0 ]
 then
-	echo $a" -> packer : fail"
+	echo $result" -> packer : fail"
+	exit $result
+fi
+
+./woody > /tmp/test_woody
+result=$?
+if [ $result -gt 128 ]
+then
+	echo $result" -> woody : fail"
+	exit $result
+fi
+if [ -n "$(diff -a <(cat /tmp/test_woody | tail -c +13) <(/tmp/$1))" ]
+then
+	echo "$1 :" >> /tmp/result
+	echo "compil : gcc $2 -o /tmp/_test_woody_$1 testsuite/src/$1.c" >>/tmp/result
+	echo >> /tmp/result
+	echo result >> /tmp/result
+	cat /tmp/test_woody >> /tmp/result
+	echo >> /tmp/result
+	echo diff >> /tmp/result
+	diff -a <(cat a | tail -c +13) <(/tmp/$1) >> /tmp/result
+	echo >> /tmp/result
+	echo >> /tmp/result
+	echo "exec : fail"
+	cp woody woody$2
+	
 	exit 1
 fi
-
-./woody > a
-a=$?
-if [ $a -gt 128 ]
-then
-	echo $a" -> woody : fail"
-	exit $a
-fi
-
-touch t #diff ls
-
-if [ -n "$(diff -a <(cat a | tail -c +16) <(/tmp/$1))" ]
-then
-	echo $a" -> exec : fail"
-	exit 1
-fi
-rm /tmp/_test_woody_$1
+rm /tmp/test_woody
+rm /tmp/$1
+rm woody
 exit 0
+
